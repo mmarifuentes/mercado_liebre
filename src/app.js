4ae1c5a762c310d4e01050e55c8b42db60e0c0a2
mmarifuentes <maria.fuentes1203@gmail.com>
2023-07-27 00:36:42 -0300

    agregando la parte de html

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express= require("express");
+const app = express();
+const path= require("path"); 
+
+app.use(express.static("public"));
+
+app.listen(3030, () => console.log("servidor corriendo en el puerto 3030"));
+
+app.get("/", (req,res) => { res.sendFile(path.join(__dirname, "./views/home.html"))  
+});
