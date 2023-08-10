8beaf86366e088bfc43e4cac6879ff5dc753fb2e
mmarifuentes <maria.fuentes1203@gmail.com>
2023-08-10 16:36:59 -0300

    agrego login y cambios en home

--- src/app.js
+++ src/app.js
@@ -10,3 +10,9 @@ app.listen(port, () => console.log("servidor corriendo en el puerto 3001"));
 
 app.get("/", (req,res) => { res.sendFile(path.join(__dirname, "./views/home.html"))  
 });
+
+app.get("/registro", (req,res) => { res.sendFile(path.join(__dirname, "./views/register.html"))  
+});
+
+app.get("/login", (req,res) => { res.sendFile(path.join(__dirname, "./views/login.html"))  
+});
